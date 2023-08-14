e16713cbee1f4dabe755625d4b9314bdcfedba4e
nasohi <ciptandani@gmail.com>
2023-08-14 14:33:34 +0700

    Update network.tf

--- terraform/infra/network.tf
+++ terraform/infra/network.tf
@@ -33,29 +33,29 @@ resource "aws_route_table_association" "rta_subnet_public" {
   route_table_id = aws_route_table.rtb_public.id
 }
 
-resource "aws_security_group" "nshcloudlabs-public-security" {
-  name   = "nshcloudlabs-public-security"
-  vpc_id = aws_vpc.vpc.id
-
+#resource "aws_security_group" "nshcloudlabs-public-security" {
+#  name   = "nshcloudlabs-public-security"
+#  vpc_id = aws_vpc.vpc.id
+#
   # SSH access from the VPC
-  ingress {
-    from_port   = 2022
-    to_port     = 2022
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 2022
+#    to_port     = 2022
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
   
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 }
